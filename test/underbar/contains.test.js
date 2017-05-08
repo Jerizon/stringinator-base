9716e1315e115d99bf4b53aac64ece9b40977354
Jerizon <JesseLowellReyes@gmail.com>
2017-05-08 13:55:18 -0700

    "Add test for underbar functions contains, every, and some"

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,29 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    expect(_.contains(arr, 'c')).toEqual(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    expect(_.contains(arr, 'f')).toEqual(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    const obj = {
+      a: 'I fell in love with a Starship Trooper',
+      b: 'Sarah Brightman and Hot Gossip'
+    }
+    expect(_.contains(obj, 'I fell in love with a Starship Trooper')).toEqual(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    const obj = {
+      a: 'I fell in love with a Starship Trooper',
+      b: 'Sarah Brightman and Hot Gossip'
+    }
+    expect(_.contains(obj, 'Video killed the Radio Star')).toEqual(false);
   });
 
 });
--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,13 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    const arr = [1, 2, 4, 6, 8]
+    expect(_.some(arr, item => item % 2 === 1)).toEqual(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    const arr = [2, 4, 6, 8]
+    expect(_.some(arr, item => item % 2 === 1)).toEqual(false);
   });
 
 });
