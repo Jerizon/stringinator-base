0edd5e1d5414a761ba7b783a3de4ebe69f3163da
Jerizon <JesseLowellReyes@gmail.com>
2017-05-08 13:57:25 -0700

    "Add tests for the AMAZING underbar function reduce"

--- test/underbar/reduce.test.js
+++ test/underbar/reduce.test.js
@@ -2,11 +2,31 @@ const _ = require('../../underbar');
 
 describe('reduce()', () => {
   it('reduces an array of numbers to a sum, *with* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+    let numArr = [1, 1, 2, 3, 5, 8, 13];
+    let result =_.reduce(numArr, (acc, num) => acc + num, 67);
+    expect(result).toEqual(100);
   });
 
   it('reduces an array of numbers to a sum, *without* an explicit initial value for the accumulator', () => {
-    // Your code goes here
+  	let numArr = [1, 1, 2, 3, 5, 8, 13];
+    expect(_.reduce(numArr, (acc, num) => acc + num)).toEqual(33);
   });
 
+    it('reduces an object with key value pairs of numbers to a sum, *with* a non-zero explicit initial value for the accumulator', () => {
+    const numsObj = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+      e: 5
+    };
+    const result = _.reduce(numsObj, (acc, num) => acc + num, 5);
+    expect(result).toEqual(20);
+  });
+
+    it('reduces an object with key value pairs of numbers to a sum, *with* a non-zero explicit initial value for the accumulator', () => {
+    const numsObj = [1, 2, 3, 4, 5];
+    const result = _.reduce(numsObj, (acc, num) => acc + num, 5);
+    expect(result).toEqual(20);
+  });
 });
