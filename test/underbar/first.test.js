10d751df068f1e7ea7bd75998e508a65ad9f6cc3
Jerizon <JesseLowellReyes@gmail.com>
2017-05-08 13:47:23 -0700

    "Rebuild Underbar and begin testing with underbar function first"

--- test/underbar/first.test.js
+++ test/underbar/first.test.js
@@ -2,14 +2,14 @@ const _ = require('../../underbar');
 
 describe('first()', () => {
   it('returns the first element of an array', () => {
-    // Your code goes here
+    expect(_.first([4, 3, 1, 8])).toBe(4);
   });
 
   it('returns the first 2 elements of an array', () => {
-    // Your code goes here
+    expect(_.first([4, 3, 1, 8], 2)).toEqual([4, 3]);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    expect(_.first([4, 3, 1, 8], 89)).toEqual([4, 3, 1, 8]);
   });
 });
